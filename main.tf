3189b499b1a6beb4503e22df42bfae36b54d9d49
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-14 13:02:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,48 @@ resource "aws_eks_cluster" "cveks" {
 
   
 }
+
+vpc_config {
+    security_group_ids      = compact(distinct(concat(var.cluster_additional_security_group_ids, [local.cluster_security_group_id])))
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = var.cluster_endpoint_private_access
+    endpoint_public_access  = var.cluster_endpoint_public_access
+    public_access_cidrs     = var.cluster_endpoint_public_access_cidrs
+  }
+
+variable "cluster_additional_security_group_ids" {
+  description = "List of additional, externally created security group IDs to attach to the cluster control plane"
+  type        = list(string)
+  default     = []
+}
+
+variable "cluster_security_group_id" {
+  description = "Existing security group ID to be attached to the cluster. Required if `create_cluster_security_group` = `false`"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs where the EKS cluster (ENIs) will be provisioned along with the nodes/node groups. Node groups can be deployed within a different set of subnet IDs from within the node group configuration"
+  type        = list(string)
+  default     = []
+}
+variable "cluster_endpoint_private_access" {
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled"
+  type        = bool
+  default     = false
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_endpoint_public_access_cidrs" {
+  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+
