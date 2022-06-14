b42817fe3360a14c348693cbd24675760ba76c4f
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-14 09:42:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   # version = "~> 2.0"
 }
 
-resource "aws_ecr_repository" "CVECR" {
+resource "aws_ecr_repository" "cvecrnon_prod" {
   name                 = "ClaimVisionECR"
   image_tag_mutability = "MUTABLE"
 
