5d6c328352c262f168a9f1c3af11ada3d27e0640
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-27 14:44:49 -0500

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -36,7 +36,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EK
 
 #Once the policies are attached, create the EKS cluster.
 
-resource "aws_eks_cluster" "cvqa" {
+resource "aws_eks_cluster" "cvdev" {
  name = "cvdev-cluster"
  role_arn = aws_iam_role.eks-iam-role.arn
 
@@ -94,8 +94,8 @@ resource "aws_iam_role" "workernodes" {
 
 
 resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.cvqa.name
-  node_group_name = "cvqa-workernodes"
+  cluster_name  = aws_eks_cluster.cvdev.name
+  node_group_name = "cvdev-workernodes"
   node_role_arn  = aws_iam_role.workernodes.arn
   subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
   instance_types = ["t3.xlarge"]
